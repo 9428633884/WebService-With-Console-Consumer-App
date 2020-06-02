722b5a1d1daa143c28fd3d72cdd9efbff71da108
Viral Shah <viralshah.04@gmail.com>
2020-06-02 15:37:06 +0530

    Add project files.

---
+++ SampleWebConsumer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SampleWebConsumer.WebServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleWebConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new WebService();
+
+            Console.Write("Square root of 64 = {0}", service.SquareRoot(64));
+        }
+    }
+}
